876e216c29c4e45a5007020a6faf8e08d0a11faf
abhay-bathani <abhaybathani3024@gmail.com>
2025-07-05 17:31:57 +0530

    String Calculator

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,20 @@
+public class StringCalculator {
+
+        public int add(String numbers) {
+
+            if (numbers == null || numbers.isEmpty()) {
+                return 0;
+            }
+
+            String[] parts = numbers.split(",");
+            int sum = 0;
+
+            for (String num : parts) {
+                sum += Integer.parseInt(num);
+            }
+
+            return sum;
+        }
+}
+
+
